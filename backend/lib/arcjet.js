28e198d705bbc1892137342d09a963f37f1d0839
tim-conoph <tjcguela@gmail.com>
2026-06-30 00:02:26 +0800

    formatting change only via prettier

--- backend/lib/arcjet.js
+++ backend/lib/arcjet.js
@@ -4,27 +4,27 @@ import "dotenv/config";
 
 //init arcjet
 export const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  characteristics: ["ip.src"],
-  rules: [
-    // shield protects ur app from common attacks (SQL Injection, XSS, CSRF Attacks)
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      // block all bots except search engines
-      allow: [
-        "CATEGORY:SEARCH_ENGINE",
-        // see full list at https://arcjet.com/bot-list
-      ],
-    }),
+    key: process.env.ARCJET_KEY,
+    characteristics: ["ip.src"],
+    rules: [
+        // shield protects ur app from common attacks (SQL Injection, XSS, CSRF Attacks)
+        shield({ mode: "LIVE" }),
+        detectBot({
+            mode: "LIVE",
+            // block all bots except search engines
+            allow: [
+                "CATEGORY:SEARCH_ENGINE",
+                // see full list at https://arcjet.com/bot-list
+            ],
+        }),
 
-    // RATE LIMITING algo
-    // one of many algos for this; can also explore SLIDING WINDOW
-    tokenBucket({
-      mode: "LIVE",
-      refillRate: 5,
-      interval: 10,
-      capacity: 10,
-    }),
-  ],
+        // RATE LIMITING algo
+        // one of many algos for this; can also explore SLIDING WINDOW
+        tokenBucket({
+            mode: "LIVE",
+            refillRate: 10,
+            interval: 10,
+            capacity: 10,
+        }),
+    ],
 });
